8d7825f18cda7f6b14cb57d15012a8440bde32ae
roman <roman@tolokoncev.tt.net>
2023-04-18 00:54:45 +0300

    merge sorted slices

--- code_wars/leet_code_2_sumFn/main.go
+++ code_wars/leet_code_2_sumFn/main.go
@@ -1,45 +1,64 @@
 package main
 
-func twoSum(nums []int, target int) []int {
-	m := map[int][]int{}
+import "fmt"
 
-	for index, el := range nums {
-		for i, e := range nums {
+// func twoSum(nums []int, target int) []int {
+// 	m := map[int][]int{}
 
-			if _, ok := m[el+e]; !ok {
-				m[el+e] = []int{}
-			}
+// 	for index, el := range nums {
+// 		for i, e := range nums {
 
-			if i == index {
-				continue
-			}
+// 			if _, ok := m[el+e]; !ok {
+// 				m[el+e] = []int{}
+// 			}
 
-			if (el + e) == target {
-				return []int{i, index}
-			}
+// 			if i == index {
+// 				continue
+// 			}
 
-		}
-	}
+// 			if (el + e) == target {
+// 				return []int{i, index}
+// 			}
 
-	return []int{}
-}
+// 		}
+// 	}
+
+// 	return []int{}
+// }
 
 func main() {
 	nums := []int{3, 2, 4}
 	target := 6
-	twoSum(nums, target)
+	fmt.Println(twoSum(nums, target))
 }
 
 // cупер умное решение
 // func twoSum(nums []int, target int) []int {
-// 	count := make(map[int]int)
-// 	for i, num := range nums {
-// 		j, ok := count[num]
+// 	lookup := make(map[int]int)
+// 	for i, v := range nums {
+// 		j, ok := lookup[-v]
+// 		lookup[v-target] = i
 // 		if ok {
 // 			return []int{j, i}
 // 		}
-// 		count[target-num] = i
 // 	}
 // 	return []int{}
-
 // }
+
+func twoSum(nums []int, target int) []int {
+	m := map[int]int{}
+
+	for i, el := range nums {
+
+		x := target - el
+
+		if j, ok := m[x]; ok {
+			return []int{j, i}
+		}
+
+		m[el] = i
+
+	}
+
+	return []int{}
+}
--- slices/merge_sort_slices/main.go
+++ slices/merge_sort_slices/main.go
@@ -3,7 +3,26 @@ package main
 import "fmt"
 
 func mergeSortSlice(a []int, b []int) []int {
+	var N = len(a)
+	var M = len(b)
+	var i int
+	var j int
+	// c := make([]int, len(a)+len(b))
+	c := []int{}
 
+	for i < N && j < M {
+		if a[i] <= b[j] {
+			c = append(c, a[i])
+			i += 1
+		}
+
+		if a[i] > b[j] {
+			c = append(c, b[j])
+			j += 1
+		}
+	}
+
+	return c
 }
 
 func main() {
